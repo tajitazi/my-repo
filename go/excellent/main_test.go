e328703669be13cb8d31c7819df128c2e6b336a4
tajitazi <tajitazi.1004@gmail.com>
2024-10-06 21:51:49 +0900

    fix: typo

--- go/excellent/main_test.go
+++ go/excellent/main_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-func TestEvenOrOdd(t *testing T) {
+func TestEvenOrOdd(t *testing.T) {
     result := EvenOrOdd(11)
     if result != "even" {
         t.Errorf("expected: even, actual: %s", result")
